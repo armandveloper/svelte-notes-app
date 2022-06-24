84becef597955b984ea51a0b2e4a67ea20ceb354
Armando Cruz <jaimes087.uz@gmail.com>
2022-06-23 21:51:09 -0500

    refactor: extract note to a component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import { nanoid } from 'nanoid';
   import NewNoteButton from './lib/components/NewNoteButton.svelte';
+  import Note from './lib/components/Note.svelte';
 
   let notes = [];
 
@@ -12,11 +13,9 @@
       )
     : notes;
 
-  function handleDeleteNote(id) {
-    return () => {
-      const newNotes = notes.filter(note => note.id !== id);
-      notes = newNotes;
-    };
+  function handleRemoveNote(event) {
+    const newNotes = notes.filter(note => note.id !== event.detail.id);
+    notes = newNotes;
   }
 
   function handleAddNote() {
@@ -46,39 +45,8 @@
   class="grid gap-x-5 gap-y-8 max-w-screen-xl mx-auto px-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
 >
   <NewNoteButton on:click={handleAddNote} />
-  {#each filteredNotes as { title, body, id } (id)}
-    <article
-      class="bg-yellow-200 p-2 rounded-md h-[15rem] transition-colors md:h-[20rem] flex flex-col"
-    >
-      <header class="flex items-center justify-end">
-        <button data-testid="remove-note" on:click={handleDeleteNote(id)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-            />
-          </svg>
-        </button>
-      </header>
-      <input
-        class="bg-transparent font-bold mb-3 outline-none"
-        type="text"
-        bind:value={title}
-      />
-      <textarea
-        class="bg-transparent text-black placeholder:text-black flex-1 outline-none resize-none "
-        placeholder="Type the body"
-        bind:value={body}
-      />
-    </article>
+  {#each filteredNotes as note (note.id)}
+    <Note {...note} on:remove={handleRemoveNote} />
   {/each}
 </main>
 
@@ -86,15 +54,5 @@
   :root {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-  textarea::-webkit-scrollbar {
-    width: 4px;
-  }
-  textarea::-webkit-scrollbar-track {
-    border-radius: 4px;
-  }
-  textarea::-webkit-scrollbar-thumb {
-    border-radius: 4px;
-    background-color: rgba(0, 0, 0, 0.5);
   }
 </style>
---
+++ src/lib/components/Note.svelte
@@ -0,0 +1,61 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  export let id;
+  export let title;
+  export let body;
+
+  const dispatch = createEventDispatcher();
+
+  function removeNote() {
+    dispatch('remove', {
+      id,
+    });
+  }
+</script>
+
+<article
+  class="bg-yellow-200 p-2 rounded-md h-[15rem] transition-colors md:h-[20rem] flex flex-col"
+>
+  <header class="flex items-center justify-end">
+    <button data-testid="remove-note" on:click={removeNote}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+        />
+      </svg>
+    </button>
+  </header>
+  <input
+    class="bg-transparent font-bold mb-3 outline-none"
+    type="text"
+    bind:value={title}
+  />
+  <textarea
+    class="bg-transparent text-black placeholder:text-black flex-1 outline-none resize-none "
+    placeholder="Type the body"
+    bind:value={body}
+  />
+</article>
+
+<style>
+  textarea::-webkit-scrollbar {
+    width: 4px;
+  }
+  textarea::-webkit-scrollbar-track {
+    border-radius: 4px;
+  }
+  textarea::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+</style>
